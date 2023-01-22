e6c399e82227f429319d53cdb41e4449e78a9c46
Lantao home PC <lantaook@gmail.com>
2023-01-21 22:07:48 -0500

    added functions and questions in two js files

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
 // TODO: Include packages needed for this application
-
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What's the project's title",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please enter the description of this project",
+  },
+  {
+    type: "input",
+    name: "installation instructions",
+    message: "Please enter the installation instructions",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please enter the usage of this project",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Please enter the contributing members",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please enter the testing instructions",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please choose the license of your project",
+    choices: ["MIT", "MPL 2.0", "ISC"],
+    },
+    {
+        type: 'input',
+        name: 'git_username',
+        message:'Please enter your github username'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message:'Please enter your email address'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,14 +1,52 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
 
+const all_license = [
+  {
+    name: "MIT",
+    badge: "https://img.shields.io/badge/License-MIT-yellow.svg",
+    link: "https://opensource.org/licenses/MIT",
+  },
+  {
+    name: "MPL 2.0",
+    badge: "https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg",
+    link: "https://opensource.org/licenses/MPL-2.0",
+  },
+  {
+    name: "ISC",
+    badge: "https://img.shields.io/badge/License-ISC-blue.svg",
+    link: "https://opensource.org/licenses/ISC",
+  },
+];
+
+function renderLicenseBadge(license) {
+  if (license.length === 0) {
+    return "";
+  } else {
+    // console.log(all_license.find((key) => key.name === license).badge);
+    return all_license.find((key) => key.name === license).badge;
+  }
+}
+renderLicenseBadge("MIT");
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license.length === 0) {
+    return "";
+  } else {
+    return all_license.find((key) => key.name === license).link;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license.length === 0) {
+    return "";
+  } else {
+    return `## License\n[${license}](${renderLicenseLink})\n`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
